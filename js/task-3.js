1877b9ac69319d703095ec3229de8adf0d70e400
hip777-art <arkhip24032000@gmail.com>
2026-02-16 23:59:27 +0200

    Add functionality to greet user based on input
    Implement input event listener to update greeting name.

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім’я для привітання. Обов’язково очищай значення в інпуті по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+// На що буде звертати увагу ментор при перевірці:
+
+// На елементі input#name-input прослуховується подія input
+// Під час набору тексту в інпуті його поточне значення підставляється в span#name-output як ім’я для привітання
+// Значення в інпуті очищене від пробілів по краях
+// Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous"
+
+const userName = document.querySelector("#name-input");
+const userNameElem = document.querySelector("#name-output");
+
+userName.addEventListener("input", onNameInput);
+
+function onNameInput() {
+  const value = userName.value.trim();
+  if (value === "") {
+    userNameElem.textContent = "Anonymous";
+  } else {
+    userNameElem.textContent = value;
+  }
+}
