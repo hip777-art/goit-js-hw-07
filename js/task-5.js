c70dd12e06632ae6cf8bcf85db8959b4b84a8a0d
hip777-art <arkhip24032000@gmail.com>
2026-02-17 00:09:14 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+// 1. Выбираем DOM-элементы
+const changeColorBtn = document.querySelector('.change-color');
+const colorText = document.querySelector('.color');
+
+// 2. Добавляем обработчик события
+changeColorBtn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+
+  // 3. Меняем цвет фона body
+  document.body.style.backgroundColor = newColor;
+
+  // 4. Отображаем текущий цвет в span.color
+  colorText.textContent = newColor;
+});
